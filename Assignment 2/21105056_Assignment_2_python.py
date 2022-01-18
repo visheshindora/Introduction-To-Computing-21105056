e5aac261de178c072701c708f860accd5c10920e
Vishesh Indora <96546258+visheshindora@users.noreply.github.com>
2022-01-19 01:38:16 +0530

    Add files via upload

---
+++ Assignment 2/21105056_Assignment_2_python.py
@@ -0,0 +1,186 @@
+# ASSIGNMENT-2
+
+
+# 1st Program
+
+
+
+print("Ques.1}\n\n")
+
+a = input("Type Your String: ")  # a = String
+# a is the string enter by user
+
+
+# Part (a)
+
+lenght = len(a)
+print("\na. Lenght of the entered string is:", lenght, "\n")
+
+
+# Part (b)
+
+rev = (a[::-1])   #Reversed
+print("b. Reversed Order of String:", rev, "\n")
+
+
+# Part (c)
+
+new_str = a[10:26]
+print("c. New String is:", new_str, "\n")
+
+
+# Part (d)
+
+X = a.replace(new_str, "object oriented")
+print("d. Repalced String is:", X, "\n")
+
+# Part (e)
+
+sub_index = a.index("a")     #Substring
+print("e. Index of Substring 'a' is:", sub_index, "\n")
+
+# Part 5
+
+no_space = a.replace(" ", "")
+print("Your String with no spaces", no_space)
+
+
+
+
+
+
+
+
+
+
+# 2nd PROGRAM
+
+
+
+print("\n\n\n\n\nQues.2}\n\n")
+
+# Data Entry By User
+
+name = input("Enter Your Name: ")
+sid = int(input("Enter Your SID: "))
+dept = input("Enter Your Department: ")
+cg = float(input("Enter Your CGPA: "))
+
+print("Hey, %s Here!\n"
+      "My SID is %s\n"
+      "I am from %s Department and My CGPA is %s" % (name,sid,dept,cg))
+
+
+
+
+
+
+
+
+
+# 3rd Program
+
+print("\n\n\n\n\nQues.3}\n\n")
+a = 56
+b = 10
+
+print("a = ", a, "\n",
+      "b = ", b)
+# Part (a)
+
+print("a. a&b = ", a & b,"\n")
+
+# Part (b)
+
+print("b. a|b = ", a | b, "\n")
+
+# Part(c)
+
+print("c. a^b = ", a ^ b, "\n")
+
+# Part (d)
+
+print("d. Left Shift of 'a' by 2 bits = ", a << 2, "\n",
+      "  Left Shift of 'b' by 2 bits = ", b << 2)
+
+# Part (e)
+
+print("e. Right Shift of 'a' by 2 bits = ", a >> 2, "\n"
+      "   Right Shift of 'b' by 4 bits = ", b >> 4)
+
+
+
+
+
+
+
+
+
+
+# Program 4
+
+
+print("\n\n\n\n\nQues.4}\n\n")
+
+print("Enter three numbers to find the greatest number:-")
+
+# Number to be enter by the user
+a = float(input("First Number = "))
+b = float(input("Second Number = "))
+c = float(input("Third Number = "))
+
+if a >= b and a >= c:
+    greatest = a
+elif b >= c:
+    greatest = b
+else:
+    greatest = c
+
+print("\nGreatest Number is:", greatest)
+
+
+
+
+
+
+
+
+
+
+# PROGRAM 5
+
+
+print("\n\n\n\n\nQues.5}\n\n")
+print("Enter a sentence to know wheather it contain 'name'or not:-\n")
+
+sen = input("Enter Your Sentence: ")  #Sentence
+remark = "Does this sentence contain word 'name' :"
+
+if "name" in sen:
+    print("\n", remark, "Yes")
+else:
+    print("\n", remark, "No")
+
+
+
+
+
+
+
+
+
+
+# PROGRAM 6
+
+print("\n\n\n\n\nQues.6}\n\n")
+
+print("Enter three lengths to se wheather a triangle can be formed from these lenghts or not:-\n")
+x = float(input("Length of First Side of Triangle = "))
+y = float(input("Length of Second Side of Trisngle = "))
+z = float(input("Length of Third Side of Triangle = "))
+
+
+if x >= y+z or y >= x+z or z >= x+y :
+     print("\nNo, Triangle can't be formed.")
+else:
+     print("\nYes, Triangle can be formed.")
---
+++ Assignment 2/21105056_Assignment_2_python_Output.py
@@ -0,0 +1,97 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: D:/Python Assignment/Assignment 2/21105056_Assignment_2_python.py ==
+Ques.1}
+
+
+Type Your String: Python is a case sensative language
+
+a. Lenght of the entered string is: 35 
+
+b. Reversed Order of String: egaugnal evitasnes esac a si nohtyP 
+
+c. New String is: a case sensative 
+
+d. Repalced String is: Python is object oriented language 
+
+e. Index of Substring 'a' is: 10 
+
+Your String with no spaces Pythonisacasesensativelanguage
+
+
+
+
+
+Ques.2}
+
+
+Enter Your Name: Vishesh Indora
+Enter Your SID: 21105056
+Enter Your Department: ECE
+Enter Your CGPA: 8.5
+Hey, Vishesh Indora Here!
+My SID is 21105056
+I am from ECE Department and My CGPA is 8.5
+
+
+
+
+
+Ques.3}
+
+
+a =  56 
+ b =  10
+a. a&b =  8 
+
+b. a|b =  58 
+
+c. a^b =  50 
+
+d. Left Shift of 'a' by 2 bits =  224 
+   Left Shift of 'b' by 2 bits =  40
+e. Right Shift of 'a' by 2 bits =  14 
+   Right Shift of 'b' by 4 bits =  0
+
+
+
+
+
+Ques.4}
+
+
+Enter three numbers to find the greatest number:-
+First Number = 45
+Second Number = 89
+Third Number = 23
+
+Greatest Number is: 89.0
+
+
+
+
+
+Ques.5}
+
+
+Enter a sentence to know wheather it contain 'name'or not:-
+
+Enter Your Sentence: Hey My name is Vishesh Indora
+
+ Does this sentence contain word 'name' : Yes
+
+
+
+
+
+Ques.6}
+
+
+Enter three lengths to se wheather a triangle can be formed from these lenghts or not:-
+
+Length of First Side of Triangle = 54
+Length of Second Side of Trisngle = 96
+Length of Third Side of Triangle = 150
+
+No, Triangle can't be formed.
